9f528b35aab3319e1e0cb6b7307b710dcedbcf27
Esteban <135396733+Jesteban247@users.noreply.github.com>
2023-10-02 17:30:20 +0000

    Cambio Quiz_2_oct

--- Docker_MySQL/sql_queries/Quiz_2_oct.sql
+++ Docker_MySQL/sql_queries/Quiz_2_oct.sql
@@ -1,15 +1,10 @@
 SELECT
     CONCAT(ad.address, ', ', ci.city) AS Tienda,
-    COUNT(DISTINCT r.rental_id) AS Numero_Prestamos,
-    AVG(p.amount) AS Pago_Promedio,
-    AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE 0 END) AS Mayo2005,
-    AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END) AS Junio2005,
-    AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END) - AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE 0 END) AS Diferencia,
-    (AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END) - AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE 0 END)) / AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE 0 END) * 100 AS Porcentaje_Crecimiento,
-    AVG(CASE WHEN MONTH(p.payment_date) = 7 THEN p.amount ELSE 0 END) AS Julio2005,
-    AVG(CASE WHEN MONTH(p.payment_date) = 7 THEN p.amount ELSE 0 END) - AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END) AS Diferencia2,
-    (AVG(CASE WHEN MONTH(p.payment_date) = 7 THEN p.amount ELSE 0 END) - AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END)) / AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE 0 END) * 100 AS Porcentaje_Crecimiento2
-
+    AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE null END) AS Mayo2005,
+    AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE null END) AS Junio2005,
+    AVG(CASE WHEN MONTH(p.payment_date) = 7 THEN p.amount ELSE null END) AS Julio2005,
+    (AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE null END) - AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE null END)) / AVG(CASE WHEN MONTH(p.payment_date) = 5 THEN p.amount ELSE null END) * 100 AS Diferencia,
+    (AVG(CASE WHEN MONTH(p.payment_date) = 7 THEN p.amount ELSE null END) - AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE null END)) / AVG(CASE WHEN MONTH(p.payment_date) = 6 THEN p.amount ELSE null END) * 100 AS Diferencia2
 FROM
     payment p
 INNER JOIN
@@ -23,10 +18,9 @@ INNER JOIN
 INNER JOIN
     city ci ON ad.city_id = ci.city_id
 WHERE
-    MONTH(p.payment_date) IN (5, 6, 7)
+    YEAR(p.payment_date) = 2005
+AND MONTH(p.payment_date) IN (5, 6, 7)
 GROUP BY
     Tienda
 ORDER BY
     Tienda;
-
-
